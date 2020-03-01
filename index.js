103e4b784fa8fd0f2d8453d03cc4cddc022a890d
vijayproject1 <vijayk.bura@gmail;.com>
2020-03-01 17:05:05 +0530

    hg

--- index.js
+++ index.js
@@ -1,50 +1,29 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-const axios = require('axios')
+const TelegramBot = require('node-telegram-bot-api');
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-) // for parsing application/x-www-form-urlencoded
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '1101047308:AAGQMrfkXCWT_qHYV-tS8pUOuBXvIYvAJpM';
 
-//This is the route the API will call
-app.post('/new-message', function(req, res) {
-  const { message } = req.body
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
 
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-  if (!message || message.text.toLowerCase().indexOf('marco') < 0) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    return res.end()
-  }
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
 
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-  axios
-    .post(
-      'https://api.telegram.org/bot1101047308:AAGQMrfkXCWT_qHYV-tS8pUOuBXvIYvAJpM/sendMessage',
-      {
-        chat_id: message.chat.id,
-        text: 'Polo!!'
-      }
-    )
-    .then(response => {
-      // We get here if the message was successfully posted
-      console.log('Message posted')
-      res.end('ok')
-    })
-    .catch(err => {
-      // ...and here if it was not
-      console.log('Error :', err)
-      res.end('Error :' + err)
-    })
-})
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
 
-// Finally, start our server
-app.listen(3000, function() {
-  console.log('Telegram app listening on port 3000!')
-})
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
